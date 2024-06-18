6f8e933eea69f3aa7d3af289704fd427576b796f
aswin0308 <aswinbaiju77@gmail.com>
2024-06-18 12:20:17 +0530

    Resolved

--- cmd/app/handler.go
+++ cmd/app/handler.go
@@ -1,189 +1,124 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 	"strconv"
 	"strings"
 	"unicode/utf8"
 )
 
-// Home renders the home page of the application.
+// Home page for the application.
 func (app *Application) Home(w http.ResponseWriter, r *http.Request) {
-	// Define template files to render
 	files := []string{
 		"ui/html/home.page.tmpl",
 		"ui/html/base.layout.tmpl",
 	}
-
-	// Retrieve splits where the user is the creator
 	s, err := app.Expense.GetYourSplit(app.Session.GetInt(r, "userId"))
-	if err != nil {
-		app.ErrorLog.Println(err)
-		log.Println(err)
+	involvedSplits, errInvolved := app.Expense.GetInvolvedSplits(app.Session.GetInt(r, "userId"))
+	if errInvolved != nil {
+		app.ErrorLog.Println()
+		log.Println(errInvolved)
 	}
 
-	// Retrieve splits where the user is involved
-	involvedSplits, err := app.Expense.GetInvolvedSplits(app.Session.GetInt(r, "userId"))
 	if err != nil {
-		app.ErrorLog.Println(err)
+		app.ErrorLog.Println()
 		log.Println(err)
 	}
-
-	// Render the page with retrieved data
 	app.render(w, files, &templateData{
-		UserId:        app.Session.GetInt(r, "userId"),
-		TitleUserName: app.Session.GetString(r, "userName"),
-		YourSplit:     s,
-		Involved:      involvedSplits,
+		UserId:    app.Session.GetInt(r, "userId"),
+		YourSplit: s,
+		Involved:  involvedSplits,
 	})
+
 }
 
-// Login renders the login page.
+// Login shows the logins page.
 func (app *Application) Login(w http.ResponseWriter, r *http.Request) {
-	// Define template files to render
 	files := []string{
 		"ui/html/login.page.tmpl",
 		"ui/html/base.layout.tmpl",
 	}
-
-	// Render the page with flash message
 	app.render(w, files, &templateData{
 		Flash: app.Session.PopString(r, "flash"),
 	})
 }
 
-// LoginUser handles user authentication.
+// Login the user after authentication.
 func (app *Application) LoginUser(w http.ResponseWriter, r *http.Request) {
-	// Define template files to render
-	files := []string{
-		"ui/html/login.page.tmpl",
-		"ui/html/base.layout.tmpl",
-	}
-
-	// Retrieve email and password from form values
 	user := r.FormValue("email")
 	password := r.FormValue("password")
 
-	// Validate email and password fields
-	if app.Validate(r, user, "email") || app.Validate(r, password, "Ispassword") {
-		flash := app.Session.PopString(r, "flash")
-		app.render(w, files, &templateData{
-			Flash: flash,
-			Email: user,
-		})
-		return
-	}
-
-	// Check if email exists in the database
-	exists, err := app.User.CheckEmail(user)
-	if err != nil {
-		app.ErrorLog.Println(err)
-		return
-	}
-
-	// Authenticate user with email and password
-	if exists {
-		id, userName, errAuth := app.User.Autenticate(user, password)
-		if errAuth != nil {
-			app.Session.Put(r, "flash", "Password is incorrect")
-			flash := app.Session.PopString(r, "flash")
-			app.render(w, files, &templateData{
-				Flash: flash,
-				Email: user,
-			})
-			app.ErrorLog.Println(errAuth)
-			log.Println(errAuth)
-			return
-		}
-		// Set session variables and redirect on successful authentication
-		app.Session.Put(r, "userId", id)
-		app.Session.Put(r, "userName", userName)
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	} else {
-		app.Session.Put(r, "flash", "Email is incorrect")
+	id, errAuth := app.User.Autenticate(user, password)
+	if errAuth != nil {
+		app.Session.Put(r, "flash", "Email or Password is incorrect")
+		app.ErrorLog.Println(errAuth)
+		log.Println(errAuth)
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
 	}
+	app.Session.Put(r, "userId", id)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-// AddUserform renders the add user form.
+// AddUserForm renders the add user form.
 func (app *Application) AddUserform(w http.ResponseWriter, r *http.Request) {
-	// Define template files to render
 	files := []string{
 		"ui/html/adduser.page.tmpl",
 		"ui/html/base.layout.tmpl",
 	}
-
-	// Render the page with flash message and username
 	app.render(w, files, &templateData{
-		Flash:         app.Session.PopString(r, "flash"),
-		TitleUserName: app.Session.GetString(r, "userName"),
+		Flash: app.Session.PopString(r, "flash"),
 	})
 }
 
-// AddUser handles adding a new user to the database.
+// AddUser adds a new user to the database.
 func (app *Application) AddUser(w http.ResponseWriter, r *http.Request) {
-	// Define template files to render
-	files := []string{
-		"ui/html/adduser.page.tmpl",
-		"ui/html/base.layout.tmpl",
-	}
-
-	// Retrieve form values
-	username := r.FormValue("username")
+	name := r.FormValue("username")
 	email := r.FormValue("email")
 	password := r.FormValue("password")
 
-	// Validate form fields
-	app.Validate(r, username, "name")
-	app.Validate(r, email, "email")
-	app.Validate(r, password, "password")
-	flash := app.Session.PopString(r, "flash")
-
-	// If there are validation errors, render the template with errors
-	if app.Validate(r, username, "name") || app.Validate(r, email, "email") || app.Validate(r, password, "password") {
-		app.render(w, files, &templateData{
-			Flash:         flash,
-			Email:         email,
-			Username:      username,
-			TitleUserName: app.Session.GetString(r, "userName"),
-		})
+	if app.Validate(r, name, "name") || app.Validate(r, email, "email") || app.Validate(r, password, "password") {
+		http.Redirect(w, r, "/adduser", http.StatusSeeOther)
 		return
 	}
 
-	// Check if username already exists
-	if !app.isValidUser(r, username) {
-		app.render(w, files, &templateData{
-			Email:         email,
-			Flash:         app.Session.PopString(r, "flash"),
-			TitleUserName: app.Session.GetString(r, "userName"),
-		})
-		return
-	}
-
-	// Check if email is valid and does not already exist
 	if !app.isValidEmail(r, email) {
-		app.render(w, files, &templateData{
-			Username:      username,
-			Flash:         app.Session.PopString(r, "flash"),
-			TitleUserName: app.Session.GetString(r, "userName"),
-		})
+		http.Redirect(w, r, "/adduser", http.StatusSeeOther)
 		return
 	}
 
-	// Insert user into the database
-	err := app.User.InsertUser(username, email, password)
+	err := app.User.InsertUser(name, email, password)
 	if err != nil {
-		app.ErrorLog.Println("Error inserting user:", err)
+		app.ErrorLog.Println(err.Error())
+		http.Redirect(w, r, "/adduser", http.StatusSeeOther)
 		return
 	}
 
-	// Redirect to /adduser on success (to clear form)
 	http.Redirect(w, r, "/adduser", http.StatusSeeOther)
 }
 
-// Validate performs field validation based on field type.
+func (app *Application) isValidEmail(r *http.Request, email string) bool {
+	regex := `^[a-zA-Z0-9._%+-]+@(gmail|yahoo)\.com$`
+	if regexp.MustCompile(regex).MatchString(email) {
+		exists, err := app.User.CheckEmail(email)
+		if err != nil {
+			app.ErrorLog.Println(err)
+			app.Session.Put(r, "flash", "An error occurred while checking the email")
+			return false
+		}
+		if exists {
+			app.Session.Put(r, "flash", "The email already exists")
+			return false
+		}
+		app.Session.Put(r, "flash", "User Successfully created")
+		return true
+	}
+	app.Session.Put(r, "flash", "The email is not valid")
+	return false
+}
+
 func (app *Application) Validate(r *http.Request, field string, fieldType string) bool {
 	switch fieldType {
 	case "name":
@@ -204,24 +139,18 @@ func (app *Application) Validate(r *http.Request, field string, fieldType string
 			app.Session.Put(r, "flash", "The password is too short (minimum is 8 characters)!")
 			return true
 		}
-	case "Ispassword":
-		if strings.TrimSpace(field) == "" {
-			app.Session.Put(r, "flash", "The password field is blank!")
-			return true
-		}
 	}
 	return false
 }
 
-// Logout handles user logout by removing session data and redirecting to login page.
+// Logout functionality.
 func (app *Application) Logout(w http.ResponseWriter, r *http.Request) {
 	app.Session.Remove(r, "userId")
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
-// AllUsers retrieves and renders a list of all users.
+// AllUsers get the list of all users.
 func (app *Application) AllUsers(w http.ResponseWriter, r *http.Request) {
-	// Retrieve list of all users
 	userlist, err := app.User.ListUsers()
 	if err != nil {
 		app.ErrorLog.Println(err.Error())
@@ -229,204 +158,134 @@ func (app *Application) AllUsers(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Define template files to render
 	files := []string{
 		"ui/html/allusers.page.tmpl",
 		"ui/html/base.layout.tmpl",
 	}
-
-	// Render the page with user list and flash message
 	app.render(w, files, &templateData{
-		UserList:      userlist,
-		Flash:         app.Session.PopString(r, "flash"),
-		TitleUserName: app.Session.GetString(r, "userName"),
+		UserList: userlist,
+		Flash:    app.Session.PopString(r, "flash"),
 	})
 }
 
-// AddSplit handles adding a new split/expense to the database.
 func (app *Application) AddSplit(w http.ResponseWriter, r *http.Request) {
-	// Parse form values
 	err := r.ParseForm()
 	if err != nil {
 		http.Error(w, "Error parsing form", http.StatusInternalServerError)
 		return
 	}
 
-	// Check if users are selected for the split
-	usersSelected := r.Form["user[]"]
-	if len(usersSelected) == 0 {
-		app.Session.Put(r, "flash", "No participants selected!")
-		http.Redirect(w, r, "/submit_expense", http.StatusSeeOther)
-		return
-	}
-
-	// Parse amount, note, and title from form values
 	amount := r.FormValue("amount")
 	amountFloat, err := strconv.ParseFloat(amount, 64)
 	if err != nil {
-		app.Session.Put(r, "flash", "Invalid amount!")
+		app.Session.Put(r, "flash", "Invalid amount !")
 		http.Redirect(w, r, "/submit_expense", http.StatusSeeOther)
 		return
 	}
 	note := r.FormValue("note")
 	title := r.FormValue("title")
 
-	// Validate title field
 	if title == "" {
-		app.Session.Put(r, "flash", "Title Required!")
+		app.Session.Put(r, "flash", "Title Required !")
 		http.Redirect(w, r, "/submit_expense", http.StatusSeeOther)
 		return
 	}
 
-	// Insert expense into the database
 	result, err := app.Expense.Insert(note, amountFloat, app.Session.GetInt(r, "userId"), title)
+	http.Redirect(w, r, "/submit_expense", http.StatusSeeOther)
 	if err != nil {
 		log.Println(err)
 		app.ErrorLog.Fatal()
 		return
 	}
+	app.Session.Put(r, "flash", "Task successfully created !")
 
-	// Retrieve last inserted expense ID
+	usersSelected := r.Form["user[]"]
+
 	expenseId, err := result.LastInsertId()
 	if err != nil {
 		app.ErrorLog.Fatal()
 	}
-
-	// Insert splits associated with the expense
 	app.Expense.Insert2Split(expenseId, amountFloat, usersSelected, app.Session.GetInt(r, "userId"))
+	http.Redirect(w, r, "/submit_expense", http.StatusSeeOther)
 
-	// Redirect to home page
-	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-// GetAddSplitForm retrieves and renders the form to add a new split/expense.
 func (app *Application) GetAddSplitForm(w http.ResponseWriter, r *http.Request) {
-	// Define template files to render
 	files := []string{
 		"ui/html/split.page.tmpl",
 		"ui/html/base.layout.tmpl",
 	}
-
-	// Retrieve all users for the split form
 	userList, errGettingList := app.User.GetAllUsers()
 	if errGettingList != nil {
 		app.ErrorLog.Fatal()
 		return
 	}
-
-	// Render the page with user data, flash message, and username
 	app.render(w, files, &templateData{
-		UserData:      userList,
-		Flash:         app.Session.PopString(r, "flash"),
-		TitleUserName: app.Session.GetString(r, "userName"),
+		UserData: userList,
+		Flash:    app.Session.PopString(r, "flash"),
 	})
+
 }
 
-// ExpenseDetails displays details of an individual expense.
+// ExpenseDetails display the details of indiviual expense
 func (app *Application) ExpenseDetails(w http.ResponseWriter, r *http.Request) {
-	// Convert expenseId from string to integer
 	expenseId, errConvert := strconv.Atoi(r.FormValue("expenseId"))
 	if errConvert != nil {
 		app.ErrorLog.Println(errConvert)
 		log.Println(errConvert)
 		return
 	}
-
-	// Retrieve expense details
-	expenseDetails, errDetails := app.Expense.ListExpensedetails(expenseId, app.Session.GetInt(r, "userId"))
+	expenseDetails, errDetails := app.Expense.ListExpensedetails(expenseId)
 	if errDetails != nil {
-		app.ErrorLog.Println(errDetails)
+		app.ErrorLog.Println()
 		log.Println("AllUsers(): ", errDetails)
 		return
 	}
 
-	// Define template files to render
 	files := []string{
 		"ui/html/expensedetails.page.tmpl",
 		"ui/html/base.layout.tmpl",
 	}
 
-	// Render the page with user ID, expense details, flash message, and username
 	app.render(w, files, &templateData{
 		UserId:         app.Session.GetInt(r, "userId"),
 		ExpenseDetails: expenseDetails,
-		Flash:          app.Session.PopString(r, "flash"),
-		TitleUserName:  app.Session.GetString(r, "userName"),
 	})
 }
 
-// MarkAsPaid handles marking an expense as paid by the user.
-func (app *Application) MarkAsPaid(w http.ResponseWriter, r *http.Request) {
-	// Retrieve expense ID from form value
-	expenseId := r.FormValue("expenseId")
-	intexpenseId, _ := strconv.Atoi(expenseId)
-
-	// Retrieve user ID from session
-	userId := app.Session.GetInt(r, "userId")
-
-	// Check if expense is already paid by the user
-	bool, err := app.Expense.CheckIfPaid(userId, intexpenseId)
-	if err != nil {
-		app.ErrorLog.Println(err)
-	}
-
-	// Redirect with flash message if already paid
-	if bool {
-		app.Session.Put(r, "flash", "You already Paid!")
-		http.Redirect(w, r, fmt.Sprintf("/expense_details?expenseId=%d", intexpenseId), http.StatusSeeOther)
-		return
-	}
-
-	// Mark expense as paid
-	err = app.Expense.Mark(userId, intexpenseId)
-	if err != nil {
-		app.ErrorLog.Println(err)
-	}
-
-	// Redirect to expense details page
-	http.Redirect(w, r, fmt.Sprintf("/expense_details?expenseId=%d", intexpenseId), http.StatusSeeOther)
-}
-
-// DeleteUser deletes a user from the database.
+// DeleteUser is to delete the user already exists.
 func (app *Application) DeleteUser(w http.ResponseWriter, r *http.Request) {
-	// Retrieve user ID from form value
 	value, errConvert := strconv.Atoi(r.FormValue("userId"))
 	if errConvert != nil {
 		log.Println(errConvert)
 		return
 	}
-
-	// Attempt to delete user
 	successDeleted, err := app.User.Delete(value)
-	if err != nil {
-		app.ErrorLog.Println(err.Error())
-		log.Println("DeleteUser(): ", err)
-		app.Session.Put(r, "Flash", "Testing.")
-		return
-	}
-
-	// Set appropriate flash message based on deletion success or involvement in splits
 	if successDeleted {
 		app.Session.Put(r, "Flash", "User deleted successfully")
 	} else if !successDeleted && err == nil {
 		app.Session.Put(r, "Flash", "User is involved in a pending split. Cannot delete the user.")
-		log.Println("User is involved in a pending split. Cannot delete the user.")
 	}
-
-	// Redirect to all users page
+	if err != nil {
+		app.ErrorLog.Println(err.Error())
+		app.Session.Put(r, "Flash", "Testing.")
+		log.Println("DeleteUser(): ", err)
+		return
+	}
+	app.Session.Put(r, "Flash", "Testing.")
+	// Redirecting to the all users page by using http.Redirect
 	http.Redirect(w, r, "/allusers", http.StatusSeeOther)
 }
 
-// Cancelexpense cancels an expense from the database.
+// Cancelexpense is to cnacel the expense that created
 func (app *Application) Cancelexpense(w http.ResponseWriter, r *http.Request) {
-	// Retrieve expense ID from form value
 	value, errConvert := strconv.Atoi(r.FormValue("expenseId"))
 	if errConvert != nil {
 		log.Println(errConvert)
 		return
 	}
 
-	// Cancel the expense
 	err := app.Expense.Cancelupdate(value)
 	if err != nil {
 		app.ErrorLog.Println(err.Error())
@@ -434,33 +293,6 @@ func (app *Application) Cancelexpense(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Redirect to home page
+	// redirect to the home page
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
-
-// Allsplits retrieves and renders all splits that the user is involved in.
-func (app *Application) Allsplits(w http.ResponseWriter, r *http.Request) {
-	// Retrieve user ID from session
-	userId := app.Session.GetInt(r, "userId")
-
-	// Define template files to render
-	files := []string{
-		"ui/html/splitList.page.tmpl",
-		"ui/html/base.layout.tmpl",
-	}
-
-	// Retrieve split transactions for the user
-	splitList, errFetchingSplitList := app.Expense.SplitList(userId)
-	if errFetchingSplitList != nil {
-		app.ErrorLog.Println(errFetchingSplitList.Error())
-		log.Println("Allsplits(): ", errFetchingSplitList)
-		return
-	}
-
-	// Render the page with split transactions, username
-	app.render(w, files, &templateData{
-		SplitTransaction: splitList,
-		TitleUserName:    app.Session.GetString(r, "userName"),
-	})
-}
-
